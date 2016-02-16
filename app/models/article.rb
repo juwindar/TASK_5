10d6c41e78773a61fb66eee13172b8908665dcb6
juwindar <juwindar.simanjuntak@gmail.com>
2016-02-16 15:57:06 +0700

    TASK_5

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true,
+                            length: { minimum: 5 }
+  validates :content, presence: true,
+                            length: { minimum: 10 }
+  validates :status, presence: true
+
+  scope :status_active, -> {where(status: 'active')}
+
+  has_many :comments, dependent: :destroy
+end
