fe0de436e85cbe892d8a01715b28396603972c0c
juwindar <juwindar.simanjuntak@gmail.com>
2016-02-19 10:09:58 +0700

    Update Telat

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
  
-  get 'static_pages/help'
-  get 'static_pages/home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  resources :static_pages
+  
+  root :to => "static_pages#home"
+  
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
-  root :to => "users#index"
+  
   resources :user
   resources :comments
   resources :photos
